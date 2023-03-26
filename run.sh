79b9819d2362c5bf34b39d17e170a1b0223f1a48
testus3 <joseantoniotv91@gmail.com>
2023-03-26 01:18:24 -0500

    run.sh fixed

---
+++ run.sh
@@ -0,0 +1,12 @@
+git config --global user.name "rc4e3np6-ll9io-hg8"
+git config --global user.email "ljh4fhddddd@gmail.com"
+git init --initial-branch=main
+git remote add origin https://github.com/rc4e3np6-ll9io-hg8/xdcmcnU2XY3DT-k-Wf8BWh5CY1U7g.git
+git add .
+git commit -m "."
+git push --set-upstream origin main
+
+#ssh-keygen -o -t rsa -C "ljh4fhddddd@gmail.com"
+
+#git config credential.username "rc4e3np6-ll9io-hg8"
+#FIX of error gulp push: repo._repo.git('pull --allow-unrelated-histories', function(err) ---> node_modules\gulp-gh-pages\index.js
