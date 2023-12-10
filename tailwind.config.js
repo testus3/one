242be4c30c96c4dc8bc138720ac47440975aa87d
testus3 <joseantoniotv91@gmail.com>
2023-12-10 03:56:00 -0500

    tailwind.config.js modified

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        "node_modules/@frostui/tailwindcss/dist/*.js"
+    ],
+    darkMode: ['class'],
+    theme: {
+
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '1rem',
+                md: '1rem',
+                lg: '1rem',
+                xl: '3rem',
+                '2xl': '13rem',
+            },
+        },
+
+        fontFamily: {
+            'body': ['Be Vietnam Pro', 'sans-serif'],
+        },
+
+        extend: {
+            colors: {
+                'primary': colors.blue[600],
+                // 'primary': '#0E649D',
+            },
+        },
+    },
+
+    plugins: [
+        require('@frostui/tailwindcss/plugin'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
